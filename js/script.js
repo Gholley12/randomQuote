20fda1685311134e39b1d94873600f3d1ea3dfcc
Gholley12 <46910574+Gholley12@users.noreply.github.com>
2019-02-10 16:39:11 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//quote array
+var quotes = [
+  {
+  quote : 'Life is about making an impact, not making an income',
+  source : 'Kevin Kruse',
+  },
+  {
+  quote : 'Whatever the mind of man can conceive and believe, it can achieve',
+  source : 'Napoleon Hill',
+  },
+  {
+  quote : 'Strive not to be a success, but rather to be of value.',
+  source : 'Albert Einstein',
+  },
+  {
+  quote : 'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.',
+  source : 'Robert Frost',
+  },
+  {
+  quote : 'I attribute my success to this: I never gave or took any excuse.',
+  source : 'Florence Nightingale',
+  },
+  {
+  quote : 'You miss 100% of the shots you don’t take.',
+  source : 'Wayne Gretzky',
+  },
+  {
+  quote : 'The most difficult thing is the decision to act, the rest is merely tenacity.',
+  source : 'Amelia Earhart',
+  },
+  {
+  quote : 'Definiteness of purpose is the starting point of all achievement.',
+  source : 'W. Clement Stone',
+  },
+  {
+  quote : 'Life is what happens to you while you’re busy making other plans.',
+  source : 'John Lennon',
+  },
+  {
+  quote : 'Life is 10% what happens to me and 90% of how I react to it.',
+  source : 'Charles Swindoll',
+  }
+];
+//variables to be filled with quote and source data
+var randomQuote = "";
+var randomSource = "";
+//function to randomly select a set of objects from the array
+function getRandomQuote(array) {
+  var rand = Math.floor(Math.random()*(quotes.length - 1));
+  randomQuote = array[rand].quote;
+  randomSource = array[rand].source;;
+}
+//function to print the selected objects to the page
+function printQuote() {
+  getRandomQuote(quotes);
+  document.getElementById("quote").innerHTML = randomQuote;
+  document.getElementById("source").innerHTML = randomSource;
+}
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
